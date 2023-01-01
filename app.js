9cd118ec4ab4bb0b7ff88fc52d624ee61d013c63
sushisyake <117505663+sushisyake@users.noreply.github.com>
2023-01-01 10:51:04 +0000

    first_commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let hello = 'Hello World';
+alert(hello);
+alert('Hello' + 'World');
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
